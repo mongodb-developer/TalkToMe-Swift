0c73dee5b8520ed68747379891c716533acb3cff
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2022-03-09 12:58:02 +0100

    Add functionality to add, read and display talks

--- TalkToMe/TalksListView.swift
+++ TalkToMe/TalksListView.swift
@@ -1,8 +1,24 @@
 import SwiftUI
+import RealmSwift
 
 struct TalksListView: View {
+    @ObservedResults(Talk.self) var talks
+    @State var counter = 1
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List (talks) { talk in
+            Text(talk.title)
+        }
+        .navigationTitle("Talks")
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Button {
+                    $talks.append(Talk(title: "Talk \(counter)", speaker: "Speaker \(counter)"))
+                    counter += 1
+                } label: {
+                    Label("Add", systemImage: "plus")
+                }
+            }
+        }
     }
 }
